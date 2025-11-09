c67d5387df5f078792d24c6e0ec06f6af2c5fd21
juan <juanancelmo109@gmail.com>
2025-11-09 13:10:45 +0000

    msmnile: build miui camera if exists

--- msmnile.mk
+++ msmnile.mk
@@ -138,6 +138,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+
+$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set,camera,package_name,com.xiaomi.sessionparams.clientName)
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
