04ef0cc6caea0cc1fbfea02da3fdc1ca41955816
momenabdulrazek <momen.abdelrazek33@gmail.com>
2025-11-19 14:16:18 +0000

    Revert "sm8150-common: Convert overlays to RRO"
    This reverts commit 36fa408fb858ab325b695089cd04a000c22fc06f.

--- msmnile.mk
+++ msmnile.mk
@@ -258,18 +258,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.android.nfc_extras.xml
 
 # Overlays
-PRODUCT_PACKAGES += \
-    CarrierConfigOverlayCommon \
-    FrameworkResOverlayCommon \
-    LineageDialerOverlayCommon \
-    LineageSDKOverlayCommon \
-    SettingsOverlayCommon \
-    SettingsProviderOverlayCommon \
-    SystemUIOverlayCommon \
-    TelephonyOverlayCommon \
-    WifiResourcesOverlayCommon
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -376,6 +370,7 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
+    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
